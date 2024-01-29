6e1be6bc0c8dd17308e710142e0ffa6b2853a896
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2024-01-29 23:17:25 +0000

    Setup react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { App } from "./App.jsx";
 
 const root = document.getElementById("root");
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
