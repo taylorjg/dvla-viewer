22f922c4b63444228326b3003d10b48bae29e80f
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2024-01-30 23:56:13 +0000

    Do simplest possible react-query / axios lookup and show results in a pre element

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+axios.defaults.baseURL =
+  "https://jen123ryri.execute-api.us-east-1.amazonaws.com";
+
+const lookupCarReg = async () => {
+  const config = {
+    params: {
+      registrationNumber: "WV18UXA",
+    },
+  };
+  const response = await axios.get("/api/lookup", config);
+  return response.data;
+};
+
 export const App = () => {
-  return <div>App</div>;
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["dvla"],
+    queryFn: lookupCarReg,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isError) return <div>Error: {error}</div>;
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 };
