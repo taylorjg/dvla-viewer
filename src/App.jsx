31b658683ec440e9cc56b33aeac0f025ba416938
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2024-01-31 15:09:08 +0000

    Improve error handling in web app

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import axios from "axios";
 axios.defaults.baseURL =
   "https://jen123ryri.execute-api.us-east-1.amazonaws.com";
 
-const lookupCarReg = async () => {
+const lookup = async (registrationNumber) => {
   const config = {
     params: {
-      registrationNumber: "WV18UXA",
+      registrationNumber,
     },
   };
   const response = await axios.get("/api/lookup", config);
@@ -15,14 +15,20 @@ const lookupCarReg = async () => {
 };
 
 export const App = () => {
+  const registrationNumber = "WV18UXA";
+
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["dvla"],
-    queryFn: lookupCarReg,
+    queryKey: ["lookup", registrationNumber],
+    queryFn: () => lookup(registrationNumber),
   });
 
   if (isLoading) return <div>Loading...</div>;
 
-  if (isError) return <div>Error: {error}</div>;
+  if (isError) {
+    const detail = error.response?.data?.error;
+    const errorMessage = detail ?? error.message;
+    return <div>Error: {errorMessage}</div>;
+  }
 
   return <pre>{JSON.stringify(data, null, 2)}</pre>;
 };
