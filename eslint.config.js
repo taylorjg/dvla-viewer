56e8554ebc418eb70959c7c777d68f82cb47c580
Jonathan Taylor <jonathan.taylor@disney.com>
2026-07-09 00:34:23 +0100

    Fix slow local lint by ignoring serverless build artefacts

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ import globals from "globals";
 
 export default [
   {
-    ignores: ["dist/**"],
+    ignores: ["dist/**", "server/dist/**", "server/.serverless/**"],
   },
   js.configs.recommended,
   eslintConfigPrettier,
